c9e392c86e9ca08ce3fc88a0d66718c68dd392c6
max-dn <max@dianacht.de>
2016-11-22 18:56:26 +0100

    läuft jetzt

--- mapnik/tools/saddledirection.sql
+++ mapnik/tools/saddledirection.sql
@@ -2,7 +2,7 @@
 -- FUNCTION INTEGER getdirection(Point:GEOMETRY in EPSG:900913)
 -- ------------------------------------------------------------
 --
--- returns the direction of a saddle at (Point) in degrees (0deg...179deg) (3 o'clock: 0deg, 12: 90deg, 9:180deg)
+-- returns the direction of a saddle at (Point) in degrees (0deg...179deg) (0:north 90:east 180:south)
 --
 -- Constants
 --    SearchArea:   limit where the next contour line must be. (not very usefull, because bounding boxes of contour lines can be huge...)
@@ -19,48 +19,93 @@
 --    if something went wrong, return -1, (-1 could be considered as error flag or as "default orientation" nearly to west-east direction)
 --
 -- Requirement:
---    you need a table "contours" with "height" and "way" (in EPSG:900913)
+--    you need a database "contours" with a table "contours" with the columns "height" and "wkb_geometry" (in EPSG:900913)
 --    
+-- Installation
+--    psql databasename < path_to_me/saddledirection.sql
+--    (as owner of the database where the saddles are, eg "gis")
+--    You also need to install the extension "dblink", but that you have allready done following "HOWTO_Preprocessing"
+
+--    For "dblink('dbname=mydb', 'select ...')" you have to be superuser or you have to provide a passwort with your query. Ig you don't like
+--    that, you could open the connection to countours with "dblink_connect_u('contours_connection', 'dbname=contours')" and then do your query
+--    with "dblink('contours_connection','select ...')". Then you just need the exection right for dblink_connect_u.
 --
+--    as postgres
+--     psql databasename -c "GRANT EXECUTE ON FUNCTION dblink_connect_u(text) to username;"
+--     psql databasename -c "GRANT EXECUTE ON FUNCTION dblink_connect_u(text,text) to username;"
+--
+
 
 CREATE OR REPLACE FUNCTION getsaddledirection(GEOMETRY) RETURNS INTEGER AS $$
 
  DECLARE
   SearchArea   CONSTANT INTEGER := 100;
-  SearchLimit  CONSTANT INTEGER :=  20;
-  MinDescent   CONSTANT INTEGER :=  30;
+  SearchLimit  CONSTANT INTEGER :=  10;
+  MinDescent   CONSTANT INTEGER :=  20;
 
-  saddlepoint  GEOMETRY := $1;
+  saddlepoint  TEXT := $1::TEXT;
   result       RECORD;
   saddleheight FLOAT;
   direction    INTEGER;
   i            INTEGER;
+  querystring  TEXT;
 
  BEGIN
   i:=0;direction:=-1;
+--
+-- open dblink-connection
+--
+  IF ('saddle_contours_connection' = ANY(dblink_get_connections())) THEN
+--   RAISE NOTICE 'connection to contours allready exists';
+  ELSE
+   PERFORM dblink_connect_u('saddle_contours_connection', 'dbname=contours');
+  END IF;
+--
+-- build query string 
+--
+  querystring='SELECT wkb_geometry,height FROM contours WHERE 
+                 st_setsrid(wkb_geometry,900913) && ST_Expand(''' || saddlepoint || '''::geometry,' || SearchArea || ') 
+                 ORDER BY ST_Distance(st_setsrid(wkb_geometry,900913),''' || saddlepoint || '''::geometry) ASC LIMIT ' || SearchLimit;
+
+--  RAISE NOTICE 'query: %s',querystring;
+
+--
+-- Get contour lines and next point to this line from saddle
+--
   <<getcontourloop>>  
-  FOR result IN ( SELECT height::FLOAT,ST_ClosestPoint(way,saddlepoint) AS cp 
-                         FROM contours 
-                         WHERE way && ST_Expand(saddlepoint,SearchArea) 
-                         ORDER BY ST_Distance(way,saddlepoint) ASC 
-                         LIMIT SearchLimit                                     ) LOOP
+  FOR result IN ( SELECT  height::FLOAT,ST_ClosestPoint(st_setsrid(way,900913),st_setsrid(saddlepoint::geometry,900913)) AS cp 
+                         FROM dblink('saddle_contours_connection',querystring) 
+                              AS t1(way geometry,height integer)
+                ) LOOP
    i:=i+1;
+--
+-- First line found defines the "height" of the saddle
+--
    IF (i=1) THEN
     saddleheight:=result.height; 
    ELSE
+--
+-- one of the next lines defines the orientation / pointing to the next lower point or 90° to the next higher point
+--
     IF (result.height<saddleheight-MinDescent) THEN
      SELECT CAST(d AS INTEGER) INTO direction FROM (SELECT degrees(ST_Azimuth(saddlepoint,result.cp)) AS d) AS foo;
-     direction:=(720-(direction-90))%180;
+     direction:=(720-(direction))%180;
+--     RAISE NOTICE 'Found lower contour line';
      EXIT getcontourloop;
     END IF;
     IF (result.height>saddleheight+MinDescent) THEN
      SELECT CAST(d AS INTEGER) INTO direction FROM (SELECT degrees(ST_Azimuth(saddlepoint,result.cp)) AS d) AS foo;
-     direction:=(720-(direction))%180;
+     direction:=(720-(direction-90))%180;
+--     RAISE NOTICE 'Found higher contour line';
      EXIT getcontourloop;
     END IF;
     
    END IF;
   END LOOP getcontourloop;
+--
+-- close dblink-connection
+--
+  PERFORM dblink_disconnect('saddle_contours_connection');
   RETURN direction;
  END;
 $$ LANGUAGE plpgsql;
