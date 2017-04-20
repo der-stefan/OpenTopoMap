00090bd1f717c15b0ee2c0f7217531e4dc4e8325
Max Berger <max@dianacht.de>
2017-04-20 08:20:24 +0200

    Update stationdirection.sql
    Add Layer and Terminals

--- mapnik/tools/stationdirection.sql
+++ mapnik/tools/stationdirection.sql
@@ -1,11 +1,8 @@
 --
--- FUNCTION INTEGER getstationdirection(Point:GEOMETRY in Mercator,Sztationlayer in TEXT)
--- --------------------------------------------------------------------------------------
+-- FUNCTION INTEGER getstationdirection(Point:GEOMETRY in Mercator,Stationlayer in TEXT)
+-- -------------------------------------------------------------------------------------
 --
 -- returns the direction of a station at (Point) in degrees (0deg...179deg) (0:north 90:east 180:south)
---
--- "Layer" is the layer of the station, but its not used now, because we have a lot of stations at
---  layer=0 with their rails at layer=-1
 --
 -- Constants
 --    SearchArea:   limit where the next rails must be (in map units, that means "mercator-meter")
@@ -13,19 +10,18 @@
 --    RailLength:   lenght of the rails to estimate the tangent to it (in map units)
 --    ErrorReturn:  direction which is returned in case of errors
 --
---
--- Algorithm:
---    get "SearchLimit" rails next to the station (in max. "SearchArea" distance) wich are at least "RailLength" long
---    cut out a part of (1..2)*"RailLength" of each rail
---    estimate the tangent of the rails by connecting startpoint of endpoint of this parts
---    get this rail which ist "most parallel" to the other rails and take its tangent as "direction" of the station
---    
 --    -> RailLength should be big enough to select the most important rails, but not longer than the usual straight
 --        part along the platforms
 --    -> Searcharea should be small enough to get only rails belonging to this station
 --    -> SearchLimit should be a typical number of rails near a station (maybe 1..10)
 --
---    
+-- Algorithm:
+--    get "SearchLimit" rails next to the station (in max. "SearchArea" distance) wich are at least "RailLength" long
+--    cut out a part of (1..2)*"RailLength" of each rail
+--    estimate the direction of the tangent of the rails by connecting startpoint of endpoint of this parts
+--    get the direction from the station to this rail
+--    estimate from this 2 directions wether it's a terminal or not
+--
 -- Installation:
 --    psql databasename < path_to_me/stationdirection.sql
 --    (as owner of the database where the stations are, eg "gis")
@@ -40,19 +36,17 @@ CREATE OR REPLACE FUNCTION getstationdirection(point IN GEOMETRY,Stationlayer in
 
  DECLARE
   SearchArea   CONSTANT INTEGER :=  100;
-  SearchLimit  CONSTANT INTEGER :=    5;
+  SearchLimit  CONSTANT INTEGER :=    8;
   RailLength   CONSTANT INTEGER :=   30;
   ErrorReturn  CONSTANT INTEGER :=    0;
 
   result          RECORD;
   direction       INTEGER;
-  raildirection   INTEGER [];
-  dirdiff         INTEGER [];
+  foundnextrail   INTEGER;
+  is_a_terminal   INTEGER;
   i               INTEGER;
-  j               INTEGER;
-  k               INTEGER;
   d               INTEGER;
-  
+  cl              INTEGER;
 
 
  BEGIN
@@ -61,56 +55,62 @@ CREATE OR REPLACE FUNCTION getstationdirection(point IN GEOMETRY,Stationlayer in
 --
 -- Loop over some rails next to the station
 --
-  <<railsloop>>  
-  FOR result IN (SELECT osm_id,railway,ST_Intersection(way,ST_Expand(ST_ClosestPoint(way,Point),RailLength)) as railpart
+  foundnextrail:=0;
+  is_a_terminal:=0;
+  <<railsloop>>
+  FOR result IN (SELECT osm_id,railway,layer,
+                        ST_Intersection(way,ST_Expand(ST_ClosestPoint(way,Point),RailLength)) as railpart,
+                        ST_ClosestPoint(way,Point) as closestpoint
                         FROM  planet_osm_line AS t1
                         WHERE ST_DWithin(way,Point,SearchArea)
-                        AND   railway IN ('funicular','light_rail','monorail','narrow_gauge','rail','disused','abandoned','preserved') 
+                        AND   railway IN ('funicular','light_rail','monorail','narrow_gauge','rail','disused','abandoned','preserved','subway')
                         AND   ST_Length(way)>2*RailLength
---                        AND   (layer=Stationlayer or (Stationlayer is null and layer is null))
                         ORDER BY st_distance(way,Point) ASC
-                        LIMIT SearchLimit) 
+                        LIMIT SearchLimit)
                         LOOP
    i:=i+1;d:=0;
 --
--- get the tangent of the rail
+-- get the direction d of "tangent" of the rail and the direction cl of the line from the station to this rail
 --
-   SELECT CAST(az AS INTEGER) INTO d FROM (SELECT Degrees(ST_Azimuth(ST_StartPoint(result.railpart),ST_EndPoint(result.railpart)))as az) AS foo;
+   SELECT CAST(az AS INTEGER) INTO d  FROM (SELECT Degrees(ST_Azimuth(ST_StartPoint(result.railpart),ST_EndPoint(result.railpart)))as az) AS foo;
+   SELECT CAST(az AS INTEGER) INTO cl FROM (SELECT Degrees(ST_Azimuth(Point,result.closestpoint))as az) AS foo;
    d:=(360+d)%180;
-   raildirection[i]:=d;
---   RAISE NOTICE '% osm_id=%  railway=% degrees=%',i,result.osm_id,result.railway,raildirection[i];
-   IF (i=1) THEN 
-    direction:=d; 
+   cl=(360+cl)%180;
+--
+-- a station is a terminal of a rail, if the angle between d and cl is not about 90 degrees
+-- if (cl=NULL) the station is part of the rail and it is not a terminal
+--
+   is_a_terminal:=0;
+   IF ((cl IS NOT NULL) AND ((abs(d-cl)<45) OR (abs(d-cl)>135))) THEN is_a_terminal:=1; END IF;
+--   RAISE NOTICE '% osm_id=%  railway=% layer=% degrees=% shortestline=% terminal=%',i,result.osm_id,result.railway,result.layer,d,cl,is_a_terminal;
+--
+-- the first rail is taken, because we don't know if there will come better rails
+-- that's a fall back if we don't get rails with same layer as the station
+--
+   IF (i=1) THEN
+    direction:=d;
+    IF (is_a_terminal = 1) THEN direction=(direction+90)%180; END IF;
+   END IF;
+--
+-- the first rail with the same layer as the station is taken
+-- if the station is a terminal it is rotated by 90 degrees
+--
+   IF ((foundnextrail=0) AND ((result.layer = Stationlayer) OR (Stationlayer IS NULL AND result.layer IS NULL))) THEN
+--    RAISE NOTICE '% take that rail',i;
+    foundnextrail:=1;
+    direction:=d;
+    IF (is_a_terminal = 1) THEN direction=(direction+90)%180; END IF;
    END IF;
   END LOOP railsloop;
---
--- get the "best" rail: mybe we should do some clustering ...
---
-  IF (i>2) THEN
-   j:=1;
-   WHILE (j<=i) LOOP
-    k:=1;
-    dirdiff[j]:=0;
-    WHILE (k<=i) LOOP
-     dirdiff[j]:=dirdiff[j]+abs(raildirection[j]-raildirection[k])%90;
---     RAISE NOTICE 'dirdiff %-%: %',j,k,dirdiff[j];
-     k:=k+1;
-    END LOOP;
-    j:=j+1;
-   END LOOP;
-   direction:=raildirection[1];
-   j:=1;k:=1;
-   WHILE (j<=i) LOOP
-    IF (dirdiff[j]<dirdiff[k]) THEN
-     direction:=raildirection[j];
---     RAISE NOTICE 'dir % won',j;
-     k:=j;
-    END IF;
-    j:=j+1;
-   END LOOP;
-  END IF;
   RETURN direction;
  END;
 $$ LANGUAGE plpgsql;
 
-SELECT name,railway,getstationdirection(way,layer) as direction from planet_osm_point where osm_id=4436626591;
+-- Tests
+-- SELECT name,railway,layer,getstationdirection(way,layer) as direction from planet_osm_point where osm_id=4436626591;
+-- SELECT name,railway,layer,getstationdirection(way,layer) as direction from planet_osm_point where osm_id=58822410;
+-- SELECT name,railway,layer,getstationdirection(way,layer) as direction from planet_osm_point where osm_id=2499357757;
+-- SELECT name,railway,layer,getstationdirection(way,layer) as direction from planet_osm_point where osm_id=286674799;
+-- SELECT name,railway,layer,getstationdirection(way,layer) as direction from planet_osm_point where osm_id=3090733718;
+-- SELECT name,railway,layer,getstationdirection(way,layer) as direction from planet_osm_point where osm_id=327613695;
+-- SELECT name,railway,layer,getstationdirection(way,layer) as direction from planet_osm_point where osm_id=3419908160;
