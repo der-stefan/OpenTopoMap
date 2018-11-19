df465e25d963d6f18f31fb125c04cb8dcb33f6bb
der-stefan <stefan_erhardt@gmx.de>
2018-11-19 22:18:40 +0100

    delete unnecessary script

--- garmin/tools/mkgmap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-java -jar /usr/src/mkgmap-r4104/mkgmap.jar splitter-out/*.pbf --output-dir=mkgmap-out/ --gmapsupp --dem=/home/otmuser/garmintest/hgt/ --dem-dists=3312,13248,26512,53024 --verbose --dem-poly=mittelfranken.poly  --show-profiles=1 --overview-dem-dist=55000
-
-# -c git/OpenTopoMap/garmin/opentopomap_options  --style-file=./git/OpenTopoMap/garmin/style/opentopomap git/OpenTopoMap/garmin/style/typ/OpenTopoMap.typ
