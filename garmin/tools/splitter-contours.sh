5b23740d3e98bd2d985b7733b0d2a0409fa4da13
der-stefan <stefan_erhardt@gmx.de>
2019-04-13 21:37:48 +0200

    update scripts

--- garmin/tools/splitter-contours.sh
+++ garmin/tools/splitter-contours.sh
@@ -6,12 +6,15 @@ SPLITTER_OUTPUT_DIR=$DATA_DIR/dem/contours/splitter_out
 SPLITTER_JAR=/home/garminotm/src/splitter-r592/splitter.jar
 
 #continents="africa antarctica asia australia-oceania central-america europe north-america south-america"
-#continents="australia-oceania"
+continents="africa antarctica asia australia-oceania central-america north-america south-america"
 #continents="africa"
-#continents="south-america"
-#continents="asia north-america"
+#continents="antarctica"
+#continents="asia"
+#continents="australia-oceania"
 #continents="central-america"
-continents="europe"
+#continents="europe"
+#continents="south-america"
+#continents="north-america"
 
 for c in $continents
 do
@@ -19,6 +22,6 @@ do
 
     mkdir -p $SPLITTER_OUTPUT_DIR/$c-splitter-out
 
-    java -Xmx10000m -jar $SPLITTER_JAR $DATA_DIR/dem/contours-$c.pbf  --output-dir=$SPLITTER_OUTPUT_DIR/$c --max-threads=32 --mapid=53350001
+    java -Xmx10000m -jar $SPLITTER_JAR $DATA_DIR/dem/contours/contours-$c.pbf  --output-dir=$SPLITTER_OUTPUT_DIR/$c-splitter-out --max-threads=32 --mapid=53350001
 
 done
