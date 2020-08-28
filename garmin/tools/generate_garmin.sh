9688972758df43653d1ffc2955af2a10c6fd49b9
der-stefan <stefan_erhardt@gmx.de>
2020-08-28 16:40:35 +0200

    make Garmin toolchain functioning again...

--- garmin/tools/generate_garmin.sh
+++ garmin/tools/generate_garmin.sh
@@ -8,8 +8,8 @@ GIT_DIR=/home/garminotm/OpenTopoMap/garmin
 DATA_DIR=/home/garminotm/garmin_world
 
 # Programs
-SPLITTER_JAR=/home/garminotm/src/splitter-r592/splitter.jar
-MKGMAP_JAR=/home/garminotm/src/mkgmap-r4284/mkgmap.jar
+SPLITTER_JAR=/home/garminotm/src/splitter-r597/splitter.jar
+MKGMAP_JAR=/home/garminotm/src/mkgmap-r4565/mkgmap.jar
 TILESINPOLY_CMD=$GIT_DIR/tools/tiles_in_poly.py
 
 # Temp dirs
@@ -28,7 +28,7 @@ MKGMAP_TYP_FILE=$GIT_DIR/style/typ/OpenTopoMap.txt
 BOUNDS_FILE=$DATA_DIR/bounds-latest.zip
 SEA_FILE=$DATA_DIR/sea-latest.zip
 DEM_FILE=$DATA_DIR/dem/viewfinderpanoramas.zip
-WWW_OUT_ROOT_DIR=/var/www/otm_garmin/www/data
+WWW_OUT_ROOT_DIR=/var/www/garmin
 
 
 if [ ! -d $SPLITTER_OUTPUT_ROOT_DIR ]
@@ -42,7 +42,7 @@ then
 fi
 
 continents="africa antarctica asia australia-oceania central-america north-america south-america"
-#continents="europe"
+#continents="central-america"
 
 for continent in $continents
 do
@@ -50,6 +50,7 @@ do
 	wget -N http://download.geofabrik.de/$continent-latest.osm.pbf -P $DATA_DIR
 	
 	echo "Split $continent..."
+	rm -rf $SPLITTER_OUTPUT_ROOT_DIR/$continent
 	mkdir -p $SPLITTER_OUTPUT_ROOT_DIR/$continent
     java -Xmx10000m -jar $SPLITTER_JAR $DATA_DIR/$continent-latest.osm.pbf  --output-dir=$SPLITTER_OUTPUT_ROOT_DIR/$continent --max-threads=32 --geonames-file=$DATA_DIR/cities15000.txt --mapid=53530001 > $SPLITTER_OUTPUT_ROOT_DIR/$continent/splitter.log
 	
@@ -78,5 +79,6 @@ do
 		rm $MKGMAP_OUTPUT_DIR/53*.img $MKGMAP_OUTPUT_DIR/53*.tdb $MKGMAP_OUTPUT_DIR/ovm*.img $MKGMAP_OUTPUT_DIR/*.typ
 		#mv $MKGMAP_OUTPUT_DIR/gmapsupp.img $MKGMAP_OUTPUT_DIR/otm-$countryname.img
 		mv $MKGMAP_OUTPUT_DIR/gmapsupp.img $WWW_OUT_ROOT_DIR/$continent/$countryname/otm-$countryname.img
+		touch $WWW_OUT_ROOT_DIR/$continent
 	done
 done
