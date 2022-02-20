94f7f1e7c479611a52f9517090e35a397a9ccf60
sletuffe <git@letuffe.org>
2022-02-20 23:34:13 +0100

    Here is a faster way to convert to sql queries, and in a one liner

--- mapnik/tools/update_isolations_from_topographic_isolation_viefinderpanoramas.txt.sh
+++ mapnik/tools/update_isolations_from_topographic_isolation_viefinderpanoramas.txt.sh
@@ -3,15 +3,11 @@
 
 db="gis"
 
-wget https://geo.dianacht.de/topo/topographic_isolation_viefinderpanoramas.txt
-
-for x in `cat topographic_isolation_viefinderpanoramas.txt` ; do 
-  id=`echo $x | cut -f1 -d\;` 
-  dominance=`echo $x | cut -f4 -d\;` ; 
-  echo "update planet_osm_point set otm_isolation='$dominance' where osm_id=$id;" >> update-dominance.sql 
-done 
-
-cat update-dominance.sql | psql $db
-rm update-dominance.sql
-rm topographic_isolation_viefinderpanoramas.txt
+echo "creating the otm_isolation column to hold isolation information of peaks..."
+echo "ALTER TABLE planet_osm_point ADD COLUMN otm_isolation text;" | psql $db
+echo "done"
+echo "creating an index on osm_id because osm2pgsql newer versions does no create it anymore, and we need it to update peak isolation..."
+echo "create index planet_osm_point_osm_id on planet_osm_point (osm_id);" | psql $db
+echo "done"
+wget -q https://geo.dianacht.de/topo/topographic_isolation_viefinderpanoramas.txt -O - | egrep -v '^#' | sed s/"\([0-9]*;\).*;.*;\([0-9]*\)"/"update planet_osm_point set otm_isolation=\2 where osm_id=\1"/g | psql -q $db
 
